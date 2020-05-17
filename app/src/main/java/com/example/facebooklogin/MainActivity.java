57ff7136d64b097b45f45e36caa1cc0cc5edf5de
10536006 <10836006@ntub.edu.tw>
2020-05-17 18:02:17 +0800

    成功獲取資料

--- app/src/main/java/com/example/facebooklogin/MainActivity.java
+++ app/src/main/java/com/example/facebooklogin/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +31,10 @@ import com.facebook.login.widget.LoginButton;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -36,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtName,txtEmail,txtInfo;
 
     private CallbackManager callbackManager;
+    private java.lang.Object Object;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,46 +60,92 @@ public class MainActivity extends AppCompatActivity {
         circleImageView = findViewById(R.id.profile_pic);
 
         callbackManager = CallbackManager.Factory.create();
-        loginButton.setPermissions(Arrays.asList("email","public_profile"));
-        checkLoginStatus();
-
-
+        loginButton.setPermissions(Arrays.asList("public_profile","email"));
+        //--new----------------------------
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
+                String accessToken = loginResult.getAccessToken().getToken();
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.d("response",response.toString());
+                        getData(object);
 
-                /*
+                        Toast.makeText(MainActivity.this,"登入成功",Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent();
+                        intent.setClass(MainActivity.this ,GetUserName.class );
+                        startActivity(intent);
+                    }
+                });
+
+                //request Graph API
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,email,name");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+            @Override
+            public void onCancel() {}
+            @Override
+            public void onError(FacebookException error) {}
+        });
+        //---endofNew---------------------------
+        //checkLoginStatus();
+        //---------------------------
+        printKeyHash();
+        /*
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
                 txtInfo.setText("使用者id： "+loginResult.getAccessToken().getUserId());
                 String image_url = "https://graph.facebook.com/"+loginResult.getAccessToken().getUserId()+"/picture?return_ssl_resource=1";
                 Glide.with(MainActivity.this).load(image_url).into(circleImageView);
-                */
-
 
                 Toast.makeText(MainActivity.this,"登入成功",Toast.LENGTH_LONG).show();
                 Intent intent = new Intent();
                 intent.setClass(MainActivity.this ,GetUserName.class );
                 startActivity(intent);
-
             }
-
             @Override
-            public void onCancel() {
-
-            }
-
+            public void onCancel() {}
             @Override
-            public void onError(FacebookException error) {
-
-            }
+            public void onError(FacebookException error) {}
         });
+        */
+
+        //---new-----------------------------
+        //If 已經登入
+        if (AccessToken.getCurrentAccessToken() != null){
+            txtInfo.setText(AccessToken.getCurrentAccessToken().getUserId());
+
+        }
+        //---newdofnew-----------------------------
+
     }
+
+    private void getData(JSONObject object) {
+        try {
+            URL profile_picture = new URL("https://graph.facebook.com/"+object.getString("id")+"/picture?width=250&height=250");
+            Glide.with(MainActivity.this).load(profile_picture.toString()).into(circleImageView);
+
+            txtEmail.setText(object.getString("email"));
+            txtName.setText(object.getString("name"));
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         callbackManager.onActivityResult(requestCode,resultCode,data);
         super.onActivityResult(requestCode, resultCode, data);
     }
-
+    /*
     AccessTokenTracker tokenTracker =new AccessTokenTracker() {
         @Override
         protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
@@ -104,7 +160,8 @@ public class MainActivity extends AppCompatActivity {
                 loadUserProfile(currentAccessToken);
         }
     };
-
+    */
+    /*
     //--試試試試看
     private void loadUserProfile(AccessToken accessToken){
         GraphRequest request = GraphRequest.newMeRequest(
@@ -164,12 +221,32 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        */
-    }
 
+    }
+    */
+    /*
     private void checkLoginStatus(){
         if (AccessToken.getCurrentAccessToken() != null){
             loadUserProfile(AccessToken.getCurrentAccessToken());
+        }
+    }
+    */
+
+
+
+    //----------------------------------------
+    private void printKeyHash(){
+        try {
+            @SuppressLint("PackageManagerGetSignatures") PackageInfo info = getPackageManager().getPackageInfo("com.example.facebooklogin", PackageManager.GET_SIGNATURES);
+            for (Signature  signature:info.signatures){
+                MessageDigest md =MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(),Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
         }
     }
 }
