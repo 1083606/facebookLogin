36b8cb198bb8d8c42dd16d67a5364ad257e77297
Zark <10836019@ntub.edu.tw>
2020-11-10 15:04:24 +0800

    11/10版暫存

--- app/src/main/java/com/example/facebooklogin/ChatActivity.java
+++ app/src/main/java/com/example/facebooklogin/ChatActivity.java
@@ -1,173 +1,59 @@
 package com.example.facebooklogin;
 
-import androidx.annotation.CallSuper;
-import androidx.annotation.UiThread;
 import android.os.Bundle;
-import android.os.Handler;
-import com.example.facebooklogin.bean.Message;
-import android.view.View;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
-import com.example.facebooklogin.bean.MsgSendStatus;
-import com.example.facebooklogin.bean.MsgType;
-import com.example.facebooklogin.bean.TextMsgBody;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import com.example.facebooklogin.Response;
+import com.example.facebooklogin.ChatAdapter;
 
-public class ChatActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+public class ChatActivity extends AppCompatActivity {
 
-    @BindView(R.id.llContent)
-    LinearLayout mLlContent;
-    @BindView(R.id.rv_chat_list)
-    RecyclerView mRvChat;
-    @BindView(R.id.et_content)
-    EditText mEtContent;
-    @BindView(R.id.bottom_layout)
-    RelativeLayout mRlBottomLayout;//表情,添加底部布局
-    @BindView(R.id.ivAdd)
-    ImageView mIvAdd;
-    @BindView(R.id.btn_send)
-    StateButton mBtnSend;//发送按钮
-    LinearLayout mLlEmotion;//表情布局
-    @BindView(R.id.llAdd)
-    LinearLayout mLlAdd;//添加布局
-    @BindView(R.id.swipe_chat)
-    SwipeRefreshLayout mSwipeRefresh;//下拉刷新
-    private ChatAdapter mAdapter;
-    public static final String 	  mSenderId="right";
-    public static final String     mTargetId="left";
-    public static final int       REQUEST_CODE_IMAGE=0000;
-    public static final int       REQUEST_CODE_VEDIO=1111;
-    public static final int       REQUEST_CODE_FILE=2222;
-
+    EditText userInput;
+    RecyclerView recyclerView;
+    ChatAdapter messageAdapter;
+    List<Response> responseMessageList;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        initContent();
-    }
+        userInput = findViewById(R.id.userInput);
+        recyclerView = findViewById(R.id.conversation);
+        responseMessageList = new ArrayList<>();
+        messageAdapter = new ChatAdapter(responseMessageList, this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
+        recyclerView.setAdapter(messageAdapter);
 
-
-    private    ImageView  ivAudio;
-
-    protected void initContent() {
-        ButterKnife.bind(this) ;
-        mAdapter=new ChatAdapter(this, new ArrayList<com.example.facebooklogin.bean.Message>());
-        LinearLayoutManager mLinearLayout=new LinearLayoutManager(this);
-        mRvChat.setLayoutManager(mLinearLayout);
-        mRvChat.setAdapter(mAdapter);
-        mSwipeRefresh.setOnRefreshListener(this);
-        initChatUi();
-    }
-
-    @Override
-    public void onRefresh() {
-        //下拉刷新模拟获取历史消息
-        List<Message> mReceiveMsgList=new ArrayList<Message>();
-        //构建文本消息
-        Message mMessgaeText=getBaseReceiveMessage(MsgType.TEXT);
-        TextMsgBody mTextMsgBody=new TextMsgBody();
-        mTextMsgBody.setMessage("收到的消息");
-        mReceiveMsgList.add(mMessgaeText);
-    }
-
-    private void initChatUi(){
-        //底部布局弹出,聊天列表上滑
-        mRvChat.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+        userInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-                if (bottom < oldBottom) {
-                    mRvChat.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (mAdapter.getItemCount() > 0) {
-                                mRvChat.smoothScrollToPosition(mAdapter.getItemCount() - 1);
-                            }
-                        }
-                    });
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_SEND) {
+                    Response responseMessage = new Response(userInput.getText().toString(), true);
+                    responseMessageList.add(responseMessage);
+                    Response responseMessage2 = new Response(userInput.getText().toString(), false);
+                    responseMessageList.add(responseMessage2);
+                    messageAdapter.notifyDataSetChanged();
+                    if (!isLastVisible())
+                        recyclerView.smoothScrollToPosition(messageAdapter.getItemCount() - 1);
                 }
+                return false;
             }
         });
     }
-
-    @OnClick({R.id.btn_send})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.btn_send:
-                sendTextMsg(mEtContent.getText().toString());
-                mEtContent.setText("");
-                break;
-        }
-    }
-
-    //文本消息
-    private void sendTextMsg(String hello)  {
-        final Message mMessgae=getBaseSendMessage(MsgType.TEXT);
-        TextMsgBody mTextMsgBody=new TextMsgBody();
-        mTextMsgBody.setMessage(hello);
-        //开始发送
-        mAdapter.addData(hello);
-        //模拟两秒后发送成功
-        updateMsg(mMessgae);
-    }
-
-    private Message getBaseSendMessage(MsgType msgType){
-        Message mMessgae=new Message();
-        mMessgae.setUuid(UUID.randomUUID()+"");
-        mMessgae.setSenderId(mSenderId);
-        mMessgae.setTargetId(mTargetId);
-        mMessgae.setSentTime(System.currentTimeMillis());
-        mMessgae.setSentStatus(MsgSendStatus.SENDING);
-        mMessgae.setMsgType(msgType);
-        return mMessgae;
-    }
-
-
-    private Message getBaseReceiveMessage(MsgType msgType){
-        Message mMessgae=new Message();
-        mMessgae.setUuid(UUID.randomUUID()+"");
-        mMessgae.setSenderId(mTargetId);
-        mMessgae.setTargetId(mSenderId);
-        mMessgae.setSentTime(System.currentTimeMillis());
-        mMessgae.setSentStatus(MsgSendStatus.SENDING);
-        mMessgae.setMsgType(msgType);
-        return mMessgae;
-    }
-
-
-    private void updateMsg(final Message mMessgae) {
-        mRvChat.scrollToPosition(mAdapter.getItemCount() - 1);
-        //模拟2秒后发送成功
-        new Handler().postDelayed(new Runnable() {
-            public void run() {
-                int position=0;
-                mMessgae.setSentStatus(MsgSendStatus.SENT);
-                //更新单个子条目
-                for (int i=0;i<mAdapter.getData().size();i++){
-                    Message mAdapterMessage=mAdapter.getData().get(i);
-                    if (mMessgae.getUuid().equals(mAdapterMessage.getUuid())){
-                        position=i;
-                    }
-                }
-                mAdapter.notifyItemChanged(position);
-            }
-        }, 2000);
-
+    boolean isLastVisible() {
+        LinearLayoutManager layoutManager = ((LinearLayoutManager) recyclerView.getLayoutManager());
+        int pos = layoutManager.findLastCompletelyVisibleItemPosition();
+        int numItems = recyclerView.getAdapter().getItemCount();
+        return (pos >= numItems);
     }
 }
--- app/src/main/java/com/example/facebooklogin/ChatAdapter.java
+++ app/src/main/java/com/example/facebooklogin/ChatAdapter.java
@@ -1,107 +1,56 @@
 package com.example.facebooklogin;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.chad.library.adapter.base.BaseDelegateMultiAdapter;
-import com.chad.library.adapter.base.delegate.BaseMultiTypeDelegate;
-import com.chad.library.adapter.base.viewholder.BaseViewHolder;
-import com.example.facebooklogin.bean.AudioMsgBody;
-import com.example.facebooklogin.bean.FileMsgBody;
-import com.example.facebooklogin.bean.ImageMsgBody;
-import com.example.facebooklogin.bean.Message;
-import com.example.facebooklogin.bean.MsgBody;
-import com.example.facebooklogin.bean.MsgSendStatus;
-import com.example.facebooklogin.bean.MsgType;
-import com.example.facebooklogin.bean.TextMsgBody;
-import com.example.facebooklogin.bean.VideoMsgBody;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
 import java.util.List;
 
-public class ChatAdapter extends BaseDelegateMultiAdapter<Message, BaseViewHolder> {
+import com.example.facebooklogin.Response;
 
-    static final int TYPE_SEND_TEXT=1;
-    static final int TYPE_RECEIVE_TEXT=2;
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.CustomViewHolder> {
 
-    static final int SEND_TEXT = R.layout.item_text_send;
-    static final int RECEIVE_TEXT = R.layout.item_text_receive;
+    List<Response> responseMessages;
+    Context context;
+    class CustomViewHolder extends RecyclerView.ViewHolder{
+        TextView textView;
+        public CustomViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textMessage);
+        }
+    }
 
-    /*
-    private static final int SEND_LOCATION = R.layout.item_location_send;
-    private static final int RECEIVE_LOCATION = R.layout.item_location_receive;*/
-
-
-    public ChatAdapter(Context context, ArrayList<Message> data) {
-        super(data);
-        setMultiTypeDelegate(new BaseMultiTypeDelegate<Message>() {
-            @Override
-            public int getItemType(@NotNull List<? extends Message> list, int i) {
-                return 0;
-            }
-        });
-        getMultiTypeDelegate()
-                .addItemType(TYPE_SEND_TEXT, SEND_TEXT).addItemType(TYPE_RECEIVE_TEXT,RECEIVE_TEXT);
-                /*.registerItemType(TYPE_SEND_TEXT, SEND_TEXT)
-                .registerItemType(TYPE_RECEIVE_TEXT,RECEIVE_TEXT);*/
+    public ChatAdapter(List<Response> responseMessages, Context context) {
+        this.responseMessages = responseMessages;
+        this.context = context;
     }
 
     @Override
-    protected void convert(BaseViewHolder helper, Message item) {
-        setContent(helper, item);
-        setStatus(helper, item);
-        setOnClick(helper, item);
-
-    }
-
-
-    private void setStatus(BaseViewHolder helper, Message item) {
-        MsgBody msgContent = item.getBody();
-        if (msgContent instanceof TextMsgBody
-                || msgContent instanceof AudioMsgBody ||msgContent instanceof VideoMsgBody ||msgContent instanceof FileMsgBody) {
-            //只需要设置自己发送的状态
-            MsgSendStatus sentStatus = item.getSentStatus();
-            boolean isSend = item.getSenderId().equals(ChatActivity.mSenderId);
-            if (isSend){
-                if (sentStatus == MsgSendStatus.SENDING) {
-                    helper.setVisible(R.id.chat_item_progress, true).setVisible(R.id.chat_item_fail, false);
-                } else if (sentStatus == MsgSendStatus.FAILED) {
-                    helper.setVisible(R.id.chat_item_progress, false).setVisible(R.id.chat_item_fail, true);
-                } else if (sentStatus == MsgSendStatus.SENT) {
-                    helper.setVisible(R.id.chat_item_progress, false).setVisible(R.id.chat_item_fail, false);
-                }
-            }
-        } else if (msgContent instanceof ImageMsgBody) {
-            boolean isSend = item.getSenderId().equals(ChatActivity.mSenderId);
-            if (isSend) {
-                MsgSendStatus sentStatus = item.getSentStatus();
-                if (sentStatus == MsgSendStatus.SENDING) {
-                    helper.setVisible(R.id.chat_item_progress, false).setVisible(R.id.chat_item_fail, false);
-                } else if (sentStatus == MsgSendStatus.FAILED) {
-                    helper.setVisible(R.id.chat_item_progress, false).setVisible(R.id.chat_item_fail, true);
-                } else if (sentStatus == MsgSendStatus.SENT) {
-                    helper.setVisible(R.id.chat_item_progress, false).setVisible(R.id.chat_item_fail, false);
-                }
-            } else {
-
-            }
+    public int getItemViewType(int position) {
+        if(responseMessages.get(position).isMe()){
+            return R.layout.me_bubble;
         }
+        return R.layout.bot_bubble;
     }
 
-    private void setContent(BaseViewHolder helper, Message item) {
-        if (item.getMsgType().equals(MsgType.TEXT)) {
-            TextMsgBody msgBody = (TextMsgBody) item.getBody();
-            helper.setText(R.id.chat_item_content_text, msgBody.getMessage());
-        }
+    @Override
+    public int getItemCount() {
+        return  responseMessages.size();
     }
 
-
-
-    private void setOnClick(BaseViewHolder helper, Message item) {
-        MsgBody msgContent = item.getBody();
+    @Override
+    public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new CustomViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(viewType, parent, false));
     }
 
-    public void addData(String hello) {
+    @Override
+    public void onBindViewHolder(@NonNull ChatAdapter.CustomViewHolder holder, int position) {
+        holder.textView.setText(responseMessages.get(position).getText());
     }
 }
